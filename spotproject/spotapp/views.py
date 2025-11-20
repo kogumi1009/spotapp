798570566c153ab3972c381fecd3733ea1c5fb43
Joe-Takayama <jgaoshan549@gmail.com>
2025-11-20 11:45:24 +0900

    spotappにtempltesを追加しました

--- spotproject/spotapp/views.py
+++ spotproject/spotapp/views.py
@@ -1,2 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+
+class IndexView(View):
+    def get(request, self):
+        return render(request, 'spotapp/templates/base.html')
